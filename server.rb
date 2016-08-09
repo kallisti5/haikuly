b690544417a2e9ba9d39c5ff1d697dc288592f03
Alexander von Gluck IV <kallisti5@unixzen.com>
2016-08-09 18:13:09 -0500

    repo: Give details on a single repo

--- server.rb
+++ server.rb
@@ -5,6 +5,7 @@ gem 'sinatra'
 require 'sinatra'
 require 'json'
 require 'fileutils'
+require 'pp'
 
 @repo_base = "/tmp/haikuly"
 
@@ -24,8 +25,12 @@ def safe(str)
   return true
 end
 
+def repo_info(path)
+  {"name": path.split('/').last}
+end
+
 # BOOT UP
-FileUtils.mkdir_p("#{@repo_base}/public")
+FileUtils.mkdir_p("#{@repo_base}")
 
 get '/version' do
   return { api: 1, server: '1.0.0' }.to_json
@@ -33,8 +38,8 @@ end
 
 get '/repo' do
   results = Array.new
-  Dir.glob("#{settings.location}/public/*/").each do |repo|
-    results.push({"name": repo})
+  Dir.glob("#{settings.location}/*/").each do |repo|
+    results.push(repo_info(repo))
   end
   return results.to_json
 end
@@ -53,6 +58,19 @@ post '/repo' do
   FileUtils.mkdir("#{settings.location}/#{name}")
 end
 
+get '/repo/:name' do
+  if !params[:name] or !safe(params[:name])
+    status 400
+    return {error: "Invalid repo name!"}.to_json
+  end
+  name = params[:name]
+  if !Dir.exist?("#{settings.location}/#{name}")
+    status 405
+    return {error: "Repository doesn't exist!"}.to_json
+  end
+  return repo_info("#{settings.location}/#{name}").to_json
+end
+
 delete '/repo/:name' do
   if !params[:name] or !safe(params[:name])
     status 400
@@ -61,7 +79,7 @@ delete '/repo/:name' do
   name = params[:name]
   if !Dir.exist?("#{settings.location}/#{name}")
     status 405
-    return {error: "Repository already exists!"}.to_json
+    return {error: "Repository doesn't exist!"}.to_json
   end
   FileUtils.rm_rf("#{settings.location}/#{name}")
 end
